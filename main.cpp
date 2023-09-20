844a632416bfd5c5951d8d3345cd441a1174ed78
ipazzxyz <ipazzxyz@gmail.com>
2023-09-20 23:16:48 +0300

    Small changes

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 int main() {
     int a, b, c, d, x0, x1, x2;
+    std::cout << "Enter a, b, c, d and x₀, x₁, x₂." << std::endl << "Input: ";
     std::cin >> a >> b >> c >> d >> x0 >> x1 >> x2;
     RNG rng = RNG(a, b, c, d, x0, x1, x2);
-    std::cout << "P = " << rng.GetP() << std::endl;
+    std::cout << "Output:" << std::endl << "P = " << rng.GetP() << std::endl;
     std::cout << "N = " << rng.GetN() << std::endl;
     std::cout << "Z = " << rng.GetZ() << std::endl;
     return 0;
--- rng.h
+++ rng.h
@@ -1,13 +1,13 @@
 #include <iostream>
 
-inline double pow2(uint32_t num) {
+inline uint64_t pow2(uint64_t num) {
     return num * num;
 }
 
 class RNG {
 private:
     uint64_t x0_, x1_, x2_;
-    uint32_t a_, b_, c_, d_, P_ = -1, N_ = -1;
+    uint64_t a_, b_, c_, d_, P_ = -1, N_ = -1;
     double Z_ = -1;
 
     [[nodiscard]] uint64_t x(uint64_t x0, uint64_t x1, uint64_t x2) const {
@@ -15,10 +15,10 @@ private:
     }
 
 public:
-    RNG(uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint64_t x0, uint64_t x1, uint64_t x2):
+    RNG(uint64_t a, uint64_t b, uint64_t c, uint64_t d, uint64_t x0, uint64_t x1, uint64_t x2):
             a_(a), b_(b), c_(c), d_(d), x0_(x0), x1_(x1), x2_(x2) {};
 
-    uint32_t GetP() {
+    uint64_t GetP() {
         if (P_ != - 1) {
             return P_;
         }
@@ -41,7 +41,7 @@ public:
             ++P_;
         } while (x0 != y0 || x1 != y1 || x2 != y2);
 
-        for (uint32_t i = 0; i < P_ / 2; ++i) {
+        for (uint64_t i = 0; i < P_ / 2; ++i) {
             uint64_t tmp = x(x0, x1, x2);
             x0 = x1;
             x1 = x2;
@@ -56,7 +56,7 @@ public:
         return P_;
     }
 
-    uint32_t GetN() {
+    uint64_t GetN() {
         if (N_ != -1) {
             return N_;
         }
@@ -69,7 +69,7 @@ public:
         uint64_t x0 = x0_, x1 = x1_ , x2 = x2_,
                 y0 = x0_, y1 = x1_, y2 = x2_;
 
-        for (uint32_t i = 0; i < P_; ++i) {
+        for (uint64_t i = 0; i < P_; ++i) {
             uint64_t tmp = x(y0, y1, y2);
             y0 = y1;
             y1 = y2;
@@ -100,35 +100,35 @@ public:
         Z_ = 0;
 
         uint64_t x0 = x0_, x1 = x1_, x2 = x2_;
-        uint32_t N[20];
+        uint64_t N[20];
 
-        for (uint32_t & i : N) {
+        for (uint64_t & i : N) {
             i = 0;
         }
 
-        for (uint32_t i = 0; i < 397; ++i) {
-            ++N[static_cast<uint32_t>(static_cast<double>(x0) * 20 / static_cast<double>(d_))];
+        for (uint64_t i = 0; i < 397; ++i) {
+            ++N[static_cast<uint64_t>(static_cast<double>(x0) * 20 / static_cast<double>(d_))];
 
             uint64_t tmp = x(x0, x1, x2);
             x0 = x1;
             x1 = x2;
             x2 = tmp;
         }
-        ++N[static_cast<uint32_t>(static_cast<double>(x0) / static_cast<double>(d_) * 20)];
-        ++N[static_cast<uint32_t>(static_cast<double>(x1) / static_cast<double>(d_) * 20)];
-        ++N[static_cast<uint32_t>(static_cast<double>(x2) / static_cast<double>(d_) * 20)];
+        ++N[static_cast<uint64_t>(static_cast<double>(x0) / static_cast<double>(d_) * 20)];
+        ++N[static_cast<uint64_t>(static_cast<double>(x1) / static_cast<double>(d_) * 20)];
+        ++N[static_cast<uint64_t>(static_cast<double>(x2) / static_cast<double>(d_) * 20)];
 
-        for (uint32_t i : N) {
-            Z_ += pow2(i - 20) / 400.;
+        for (uint64_t i : N) {
+            Z_ += static_cast<double>(pow2(i - 20)) / 400.;
         }
 
         return Z_;
     }
 
-    [[maybe_unused]] void Print(uint32_t n) {
+    [[maybe_unused]] void Print(uint64_t n) {
         uint64_t x0 = x0_, x1 = x1_, x2 = x2_;
 
-        for (uint32_t i = 0; i < n - 3; ++i) {
+        for (uint64_t i = 0; i < n - 3; ++i) {
             std::cout << x0 << std::endl << x2 << std::endl;
             uint64_t tmp = x(x0, x1, x2);
             x0 = x1;
